90e38505e142db4a37f130c5149dedb8da0a8836
Tuhin Tarafder <2025ca93059@wilp.bits-pilani.ac.in>
2026-01-03 16:05:10 +0530

    scaffold solution and project structure

---
+++ src/FIT.Core/Goals/GoalService.cs
@@ -0,0 +1,5 @@
+﻿namespace FIT.Core.Goals;
+
+internal class GoalService
+{
+}
---
+++ src/FIT.Data/Configurations/GoalConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using FIT.Core.Goals;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FIT.Data.Configurations;
+
+internal class GoalConfiguration : IEntityTypeConfiguration<Goal>
+{
+    public void Configure(EntityTypeBuilder<Goal> builder)
+    {
+        builder.ToTable("Goals");
+
+        builder.HasKey(it => it.Id);
+        builder.Property(it => it.UserId).IsRequired();
+
+        builder.Property(it => it.TrackingMode).IsRequired();
+        builder.Property(it => it.StartDate).IsRequired();
+
+        builder.Property(it => it.TargetValue).HasPrecision(10, 2).IsRequired();
+        builder.Property(it => it.Unit).HasMaxLength(50);
+        builder.Property(it => it.Name).HasMaxLength(100);
+
+
+        builder.Property(g => g.EndDate);
+        builder.Property(g => g.IsCompleted).IsRequired();
+        builder.Property(g => g.CreatedAt).IsRequired();
+    }
+}
